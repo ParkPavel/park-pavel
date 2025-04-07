d4f4aef304858485855bf45ea63c791b9516b152
ParkPavel <naitor.krep@gmail.com>
2025-04-07 17:58:36 +0800

    Quartz sync: Apr 7, 2025, 5:58 PM

--- quartz/components/HomepageCover.tsx
+++ quartz/components/HomepageCover.tsx
@@ -1,10 +1,13 @@
 import { QuartzComponent, QuartzComponentConstructor } from "./types"
 
-const HomepageCover: QuartzComponent = () => {
+const HomepageCover: QuartzComponent = ({ cfg }) => {
+  // Убедимся, что baseUrl корректно формируется
+  const baseUrl = cfg.baseUrl?.endsWith("/") ? cfg.baseUrl : `${cfg.baseUrl}/`
+
   return (
     <div className="homepage-cover">
-      <img src="/park-pavel/static/og-imagelight.png" alt="Обложка для светлого режима" className="cover-light" />
-      <img src="/park-pavel/static/og-imagedark.png" alt="Обложка для тёмного режима" className="cover-dark" />
+      <img src={`${baseUrl}static/og-imagelight.png`} alt="Обложка для светлого режима" className="cover-light" />
+      <img src={`${baseUrl}static/og-imagedark.png`} alt="Обложка для тёмного режима" className="cover-dark" />
     </div>
   )
 }

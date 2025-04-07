37b26d3df1448f77f2ae3a4904d97ed447554d25
ParkPavel <naitor.krep@gmail.com>
2025-04-07 16:10:05 +0800

    Quartz sync: Apr 7, 2025, 4:10 PM

--- quartz/components/HomepageCover.tsx
+++ quartz/components/HomepageCover.tsx
@@ -3,8 +3,8 @@ import { QuartzComponent, QuartzComponentConstructor } from "./types"
 const HomepageCover: QuartzComponent = () => {
   return (
     <div className="homepage-cover">
-      <img src="/static/og-imagelight.png" alt="Обложка для светлого режима" className="cover-light" />
-      <img src="/static/og-imagedark.png" alt="Обложка для тёмного режима" className="cover-dark" />
+      <img src="/park-pavel/static/og-imagelight.png" alt="Обложка для светлого режима" className="cover-light" />
+      <img src="/park-pavel/static/og-imagedark.png" alt="Обложка для тёмного режима" className="cover-dark" />
     </div>
   )
 }

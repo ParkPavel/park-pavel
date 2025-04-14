fc471e278bf6692eebb7f2e13b5e94c51c9b742d
ParkPavel <naitor.krep@gmail.com>
2025-04-14 22:00:52 +0800

    Quartz sync: Apr 14, 2025, 10:00 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "G-PWSGWTQ0NR",
     },
     locale: "ru-RU",
     baseUrl: "/park-pavel/",

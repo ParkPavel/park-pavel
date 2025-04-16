0d3647db90c4caea3d086beab1acc3b34fef51e2
ParkPavel <naitor.krep@gmail.com>
2025-04-16 15:53:16 +0800

    Quartz sync: Apr 16, 2025, 3:53 PM

--- quartz/components/JivoChat.tsx
+++ quartz/components/JivoChat.tsx
@@ -1,35 +1,50 @@
 import { QuartzComponent, QuartzComponentConstructor } from "./types"
 
 const JivoChat: QuartzComponent = () => {
-  return null // No visual element needed
+  return null
 }
 
-// Add Jivo chat script after DOM is loaded
 JivoChat.afterDOMLoaded = `
-  (function() {
-    var widget_id = '5U192ufdG5';
-    var d = document;
-    var w = window;
+(function() {
+  var widget_id = '5U192ufdG5';
+  var d = document;
+  var w = window;
 
-    function l() {
-      var s = document.createElement('script');
-      s.type = 'text/javascript';
-      s.async = true;
-      s.src = '//code.jivosite.com/script/widget/' + widget_id;
-      var ss = document.getElementsByTagName('script')[0];
-      ss.parentNode.insertBefore(s, ss);
-    }
+  function l() {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = '//code.jivosite.com/script/widget/' + widget_id;
 
-    if (d.readyState == 'complete') {
-      l();
-    } else {
-      if (w.attachEvent) {
-        w.attachEvent('onload', l);
-      } else {
-        w.addEventListener('load', l, false);
+    // Добавляем изолированные стили только для JivoChat
+    var css = document.createElement('style');
+    css.type = 'text/css';
+    css.innerHTML = \`
+      jdiv[class*=__jivoMobileButton] {
+        left: 20px !important;
+        right: auto !important;
+        z-index: 90 !important;
       }
+    \`;
+    document.head.appendChild(css);
+    
+    var ss = document.getElementsByTagName('script')[0];
+    ss.parentNode.insertBefore(s, ss);
+  }
+
+  if (d.readyState === 'complete') {
+    l();
+  } else {
+    if (w.attachEvent) {
+      w.attachEvent('onload', l);
+    } else {
+      w.addEventListener('load', l, false);
     }
-  })();
+  }
+})();
 `
 
-export default (() => JivoChat) satisfies QuartzComponentConstructor
+// Правильный экспорт через QuartzComponentConstructor
+export default (() => {
+  return JivoChat
+}) satisfies QuartzComponentConstructor
